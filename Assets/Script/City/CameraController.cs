d9760b14e234291dc626a1108c82eae01780a2b6
Luisa <lbtnunes@gmail.com>
2015-11-28 20:39:22 -0200

    Create basic version of city view, with test cursor, pan and zoom

---
+++ Assets/Script/City/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void ZoomIn() {
+        if ((int)Camera.main.orthographicSize > 60)
+            Camera.main.orthographicSize -= 15; 
+    }
+
+    public void ZoomOut() {
+        if ((int)Camera.main.orthographicSize < 270)
+            Camera.main.orthographicSize += 15; 
+    }
+
+    public void ResetZoom() {
+        Camera.main.orthographicSize = 135; 
+    }
+}
---
+++ Assets/Script/City/CityCursor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CityCursor : MonoBehaviour {
+    
+    RectTransform rectTransform;
+
+    public float speed;
+    public Image background;
+
+	// Use this for initialization
+	void Start () {
+    //    UnityEngine.Cursor.visible = false;
+        rectTransform = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+    void Update() {
+        //Debug.Log(rectTransform.position.ToString());
+
+        Vector3 position = rectTransform.position;
+        Vector3 movement = new Vector3((Input.GetKey(KeyCode.LeftArrow ) ? - speed * Time.deltaTime : 0) +
+                                       (Input.GetKey(KeyCode.RightArrow) ? speed * Time.deltaTime : 0),
+                                       (Input.GetKey(KeyCode.UpArrow   ) ? + speed * Time.deltaTime : 0) +
+                                       (Input.GetKey(KeyCode.DownArrow ) ? - speed * Time.deltaTime : 0),
+                                       0);
+
+        Debug.Log(position.x + movement.x < 0 || position.x + movement.x > Screen.width);
+        Debug.Log((position.x + movement.x) + " " + (Screen.width));
+
+        Rect boundary = background.rectTransform.rect;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position + movement);
+
+        Debug.Log(boundary.ToString());
+
+        // If we're moving out of the background, do nothing
+        if (worldPosition.x + 16 > boundary.x + boundary.width ||
+            worldPosition.x - 16 < boundary.x) { }
+        // If the position of the cursor with movement applied goes outside the screen, the camera should move instead of the cursor
+        else if (position.x + movement.x < 0 || position.x + movement.x > Screen.width) {
+            Camera.main.transform.Translate(movement.x, 0, 0);
+        }
+        // Neither happened, so move the cursor
+        else {
+            rectTransform.Translate(movement.x, 0, 0);
+        }
+
+
+        if (worldPosition.y + 16 > boundary.y + boundary.height ||
+            worldPosition.y - 16 < boundary.y) { }
+        else if (position.y + movement.y < 0 || position.y + movement.y > Screen.height) {
+            Camera.main.transform.Translate(0, movement.y, 0);
+        }
+        else {
+            rectTransform.Translate(0, movement.y, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            var pointer = new PointerEventData(EventSystem.current);
+            var raycastResults = new List<RaycastResult>();
+
+            pointer.position = rectTransform.position;
+            EventSystem.current.RaycastAll(pointer, raycastResults);
+
+            if (raycastResults.Count > 0) {
+                var topmost = raycastResults[0].gameObject;
+                var b = topmost.GetComponent<Button>();
+                if (b != null) {
+                    b.onClick.Invoke();
+                }
+            }
+        }
+	}
+
+    public void Hit() {
+        Debug.Log("Hit!");
+    }
+}
